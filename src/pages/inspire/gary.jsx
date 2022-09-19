5b388f53f474a932571f26104aea379b6e6368e1
rashed <x.suwaidi@gmail.com>
2022-09-19 20:01:36 +0400

    add img and update data

--- src/pages/inspire/gary.jsx
+++ src/pages/inspire/gary.jsx
@@ -5,7 +5,7 @@ import DarkTheme from "../../layouts/Dark";
 import ProjectIntroduction from "../../components/Project-introduction/project-introduction";
 import ProjectDescription from "../../components/Project-description/project-description";
 import ProjectVideo from "../../components/Project-video/project-video";
-import Inspire from "../../data/inspire.json"
+import Inspire from "../../data/ambassadors/gary-data.json"
 import InspireDetails from "../../components/CodersHQ/Inspire/project-details-inspire.jsx";
 import InspireGallery from "../../components/CodersHQ/Inspire/project-gallery-inspire";
 
